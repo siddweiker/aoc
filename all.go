11eb86fb41a10415602cf4166d8c00d850996767
siddweiker <siddweiker@gmail.com>
2021-12-22 16:02:51 -0500

    Add day 5

--- all.go
+++ all.go
@@ -69,3 +69,17 @@ func Atoi(str string) int {
 	}
 	return i
 }
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
