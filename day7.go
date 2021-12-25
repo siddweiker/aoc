3680614f7c8a5fcfd4191e2e666639b69ec7573b
siddweiker <siddweiker@gmail.com>
2021-12-25 16:31:05 -0500

    Add day 7

---
+++ day7.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func init() {
+	Register(Day7)
+}
+
+func Day7(r io.Reader) string {
+	max := 0
+	vals := []int{}
+	scanner := bufio.NewScanner(r)
+	if scanner.Scan() {
+		line := scanner.Text()
+
+		for _, s := range strings.Split(line, ",") {
+			n := Atoi(s)
+			vals = append(vals, n)
+			if n > max {
+				max = n
+			}
+		}
+	}
+
+	fuelCosts := []int{0}
+	for i, curr := 0, 1; i <= max; i, curr = i+1, curr+i+1 {
+		fuelCosts = append(fuelCosts, i+curr)
+	}
+
+	min := max * len(vals)
+	min2 := fuelCosts[len(fuelCosts)-1] * len(vals)
+	for i := 0; i <= len(vals); i++ {
+		tot, tot2 := 0, 0
+		for _, n := range vals {
+			tot += Abs(n - i)
+			tot2 += fuelCosts[Abs(n-i)]
+		}
+		if tot < min {
+			min = tot
+		}
+		if tot2 < min2 {
+			min2 = tot2
+		}
+	}
+
+	return fmt.Sprintf("%d, %d", min, min2)
+}
